eba08a6ca24583fce94280227a06f17751b8fc09
arterro <91045842+arterro@users.noreply.github.com>
2022-08-07 22:07:38 -0700

    Add logic to remove duplicates from path

--- zsh/.config/zsh/env.sh
+++ zsh/.config/zsh/env.sh
@@ -26,21 +26,25 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 
 export GOPATH=$HOME/go
 
-if [ -d "/opt/homebrew/opt/ruby/bin" ]; then
-  export PATH=/opt/homebrew/opt/ruby/bin:$PATH
-  export PATH=$(gem environment gemdir)/bin:$PATH
-fi
-
 #**********
 # PATHS
 #**********
 
 if [[ -z $TMUX ]]; then
     path=("$HOME/.local/bin" /usr/local/{bin,sbin} $path)
-    path+=("$HOME/.emacs.d/bin" ~/bin ~/.npm-global/bin)
+    path+=("$HOME/.emacs.d/bin" "$HOME/bin" "$HOME/.npm-global/bin")
     path+=("$GOPATH/bin")
     
     if [[ $(uname -s) == "Darwin" ]]; then
         path=("/opt/homebrew/bin" $path)
+        
+        if [ -d "/opt/homebrew/opt/ruby/bin" ]; then
+            path=("$(gem environment gemdir)/bin" "/opt/homebrew/opt/ruby/bin" $path)
+        fi
     fi
 fi
+
+# Remove duplicates paths
+typeset -U path
+
+
