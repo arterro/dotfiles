ebf0a8706ab04c8a56591fdfa6c2b440c6c22229
arterro <91045842+arterro@users.noreply.github.com>
2022-08-28 21:50:47 -0700

    Add doom emacs submodule

--- _deskmod/sway/.config/sway/scripts/idle.sh
+++ _deskmod/sway/.config/sway/scripts/idle.sh
@@ -6,7 +6,6 @@ swayidle -dw \
         resume 'brightnessctl set +85%' \
     timeout 130 'swaymsg "output * dpms off"' \
             resume 'swaymsg "output * dpms on"' \
-            after-resume 'swaymsg "output * enable"'
-    #timeout 300 'swaymsg "output * dpms on"; sleep 1; swaymsg "output * enable"; sleep 1; systemctl suspend' \
-    #timeout 300 'systemctl suspend' \
-    #before-sleep 'pgrep swaylock || swaylock'
+            after-resume 'swaymsg "output * enable"' \
+    timeout 300 'systemctl suspend' \
+    before-sleep 'pgrep swaylock || swaylock'
