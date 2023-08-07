aa522b1ead5f1ad44d1d086320d36e63cf34c5ac
Arturo Delgado <commit@arter.ro>
2023-08-06 22:19:44 -0700

    Add Firefox Wayland env and packages for wireguard

--- zsh/.config/zsh/env.sh
+++ zsh/.config/zsh/env.sh
@@ -27,11 +27,16 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 
 export GOPATH=$HOME/go
 
+if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+    export MOZ_DBUS_REMOTE=1
+    export MOZ_ENABLE_WAYLAND=1
+fi
+
 #**********
 # PATHS
 #**********
 
-if [[ -z $TMUX ]]; then
+if [[ -z "$TMUX" ]]; then
     path=("$HOME/.local/bin" /usr/local/{bin,sbin} $path)
     path+=("$HOME/.emacs.d/bin" "$HOME/bin" "$HOME/.npm-global/bin")
     path+=("$GOPATH/bin")
